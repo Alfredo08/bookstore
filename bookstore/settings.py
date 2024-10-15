fa92cef827920aad37bd8effdcd24583fb962941
Alfredo08 <alfredosv08@gmail.com>
2024-10-14 22:15:23 -0600

    last commit

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','django-insecure-ml49cp(e)=yakpe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://peaceful-oasis-51298-73aea2a4ef63.herokuapp.com']
+ALLOWED_HOSTS = ['peaceful-oasis-51298-73aea2a4ef63.herokuapp.com']
 
 
 # Application definition
